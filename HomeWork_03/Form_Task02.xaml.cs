412ec0c9b00a9116aad0df9459e165f18c039839
Yurii Pizhurin <pizhurin@yahoo.com>
2020-09-10 17:34:06 +0300

    Finish Task02

--- HomeWork_03/Form_Task02.xaml.cs
+++ HomeWork_03/Form_Task02.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace HomeWork_03
@@ -19,9 +20,18 @@ namespace HomeWork_03
     /// </summary>
     public partial class Form_Task02 : Window
     {
+        double columnX = -1;
+        double rowY = -1;
+
+        int columnCount = -1;
+        int rowsCount = -1;
+
+        List<BitmapSource> bitmaps = null;
+
         public Form_Task02()
         {
             InitializeComponent();
+            bitmaps = new List<BitmapSource>();
         }
 
         private void btn_Task02_buildNewGrid_Click(object sender, RoutedEventArgs e)
@@ -38,8 +48,8 @@ namespace HomeWork_03
             
             ColumnDefinition colDef1 = new ColumnDefinition();
 
-            int columnCount = 0;
-            int rowsCount = 0;
+            columnCount = 0;
+            rowsCount = 0;
 
             if(Convert.ToInt32(textBox_Task02_columnCount.Text) != 0)
             {
@@ -63,7 +73,164 @@ namespace HomeWork_03
                 }
             }
 
+            for (int i=0; i<columnCount; i++)
+            {
+                for(int j=0; j<rowsCount; j++)
+                { 
+                    Image image = new Image();
+                    //image.Source = new BitmapImage();           // Очень долго загружает сетку         
+                    image.Source = new BitmapImage(new Uri(@"E:\temp\Image01.bmp"));
+                    image.Stretch = Stretch.Fill;
+
+                    Grid.SetColumn((UIElement)image, i);
+                    Grid.SetRow((UIElement)image, j);
+
+                    userGrid.Children.Add(image);
+                    Panel.SetZIndex(image, 3);
+                }
+            }
+
             userGrid.ShowGridLines = true;
+        }
+
+        private void userGrid_DragEnter(object sender, DragEventArgs e)
+        {
+            if((e.AllowedEffects & DragDropEffects.Copy) != 0)
+            {
+                e.Effects = DragDropEffects.Copy;                
+            }
+        }
+
+        private void userGrid_Drop(object sender, DragEventArgs e)
+        {
+            double columnCell = 0;
+            double rowCell = 0;
+
+            // Получить координаты мыши в координатах, где начался userGrid_Drop;
+            Point position = e.GetPosition(userGrid);
+            columnX = position.X;
+            l1.Content = columnX.ToString();
+            rowY = position.Y;
+            l2.Content = position.Y;
+
+            // Вычислить ячейки колонки где начался userGrid_Drop;
+            for (int i = 0; i < userGrid.ColumnDefinitions.Count; i++)
+            {
+                columnCell += userGrid.ColumnDefinitions[i].ActualWidth; 
+                if (columnCell > columnX)
+                {
+                    columnCell = i;
+                    break;
+                }
+            }
+
+            // Вычислить ячейки строки где начался userGrid_Drop;
+            for (int i = 0; i < userGrid.RowDefinitions.Count; i++)
+            {
+                rowCell += userGrid.RowDefinitions[i].ActualHeight;
+                if (rowCell > rowY)
+                {
+                    rowCell = i;
+                    break;
+                }
+            }
+
+            // Получить список перемещаемых файлов
+            string[] str = (string[])e.Data.GetData(DataFormats.FileDrop);
+            string imageName = str[0];
+            //string imageName = @"E:\temp\Image01.bmp";
+
+            // Если выделено имя файла картинки - положить картинку в контейнер
+            string ext = System.IO.Path.GetExtension(imageName);
+            if (ext == ".bmp" || ext == ".jpg" || ext == ".gif" || ext == ".png")
+            {
+                BitmapSource Source = new BitmapImage(new Uri(imageName));
+
+                bitmaps.Add(Source);
+
+                Image image = new Image();
+                image.Source = Source;
+                image.Stretch = Stretch.Fill;
+
+                Grid.SetColumn((UIElement)image, (int)columnCell);
+                Grid.SetRow((UIElement)image, (int)rowCell);
+
+                userGrid.Children.Add(image);
+                Panel.SetZIndex(image, 5);
+            }
+            //MessageBox.Show($"Количество: {bitmaps.Count}");
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // Очистить список сетки
+            if (columnCount >= 0 && rowsCount >= 0)
+            {
+                for (int i = 0; i < columnCount; i++)
+                {
+                    for (int j = 0; j < rowsCount; j++)
+                    {
+                        userGrid.Children.Clear();                       
+                    }
+                }
+            }
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            // Очистить список сетки
+            if (columnCount >= 0 && rowsCount >= 0)
+            {
+                for (int i = 0; i < columnCount; i++)
+                {
+                    for (int j = 0; j < rowsCount; j++)
+                    {
+                        userGrid.Children.Clear();
+                    }
+                }
+            }
+
+            Random random = new Random();
+            int countImages = 0;
+
+            if (columnCount >= 0 && rowsCount >= 0)
+            {
+                for (int i = 0; i < columnCount; i++)
+                {
+                    for (int j = 0; j < rowsCount; j++)
+                    {
+                        if (countImages < bitmaps.Count)
+                        {
+                            Image image = new Image();
+                            image.Source = bitmaps[countImages];
+                            image.Stretch = Stretch.Fill;
+
+                            Grid.SetColumn((UIElement)image, random.Next(0, columnCount));
+                            Grid.SetRow((UIElement)image, random.Next(0, rowsCount));
+
+                            if (!userGrid.Children.Contains(image))
+                            {
+                                userGrid.Children.Add(image);
+                                Panel.SetZIndex(image, 5);
+                                countImages++;
+                            }
+
+                            //countImages++;
+                        }
+                    }
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
--- HomeWork_03/obj/Debug/HomeWork_03_Content.g.i.cs
+++ HomeWork_03/obj/Debug/HomeWork_03_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("pin.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("target01.png")]
 
 
